c590159ba2029945cb99ae56d62b7a5f7c84f207
김진선 <sunny@MacBookAir-of-Kim.local>
2025-09-05 13:45:33 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            text: "공부하기",
+            completed: false, 
+        },
+        {
+            id: 2,
+            text: "운동하기",
+            completed: false, 
+        },
+        {
+            id: 3,
+            text: "산책하기",
+            completed: false, 
+        },  
+    ]);
+    const [addInput, setAddInput] = useState("");
+
+    const handleInputChange = (e) => {
+        setAddInput(e.target.value);
+        console.log("value", addInput);
+    };
+
+   const handleAddTodo = () => {
+    const todo = {
+        id: todos.length > 0 ? todos.at(-1).id + 1 : 1,
+        text: addInput,
+        completed: false,
+    };
+    setTodos([...todos, todo]);
+    setAddInput("");
+   };
+
+   const handDeleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+   };
+
+   const handleComplete = (id) => {
+    setTodos(
+        todos.map((todo) => 
+            todo.id === id ? {...todo, completed: !todo.completed} : todo
+        )
+    );
+   };
+
+    return (
+        <div className="App">
+            <h1>진선의 Iodo List!!</h1>
+            <div>
+                <input 
+                onChange={handleInputChange}
+                value={addInput}
+                className="add-input" 
+                type="text" 
+                />
+                <button onClick={handleAddTodo}className="add-btn">
+                    추가하기
+                </button>
+
+
+                <div className="todo-container">
+                    {todos.map((todo) => (
+                        <div key={todo.id} className="todo-item">
+                            <div
+                                style={{ textDecoration: todo.completed ? "line-through" : "none",
+                                    cursor: "pointer",
+                                 }}
+                                 onClick={() => handleComplete(todo.id)}
+                            >{todo.text}</div>
+                            <button onClick={() => handDeleteTodo(todo.id)}>삭제</button>
+                            </div>
+                        ))}
+                </div>
+            </div>
+        </div>
+    );
 }
-
-export default App;
